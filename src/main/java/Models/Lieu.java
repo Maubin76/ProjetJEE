44169b667b0a1083267fcacbd5afedd8753f624a
Mathieu Aubin <maubin@enssat.fr>
2024-03-08 18:02:51 +0100

    Model creation

---
+++ src/main/java/Models/Lieu.java
@@ -0,0 +1,41 @@
+package Models;
+
+public class Lieu {
+
+    private String nomLieu;
+    private String ville;
+    private String pays;
+    private String continent;
+
+    public String getNomLieu() {
+        return nomLieu;
+    }
+
+    public void setNomLieu(String nomLieu) {
+        this.nomLieu = nomLieu;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+}
