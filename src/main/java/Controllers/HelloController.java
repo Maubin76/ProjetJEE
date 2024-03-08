ae93c4db5fea736f6268351528d72afecf1ff548
Mathieu Aubin <maubin@enssat.fr>
2024-03-08 17:35:21 +0100

    remove unused

--- src/main/java/Controllers/HelloController.java
+++ src/main/java/Controllers/HelloController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
 import jakarta.ws.rs.*;
@@ -17,11 +16,11 @@ public class HelloController {
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/hello")
-    public void hello(@Context HttpServletRequest req, @Context HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        int count = session.getAttribute("count") == null ? 0 : (int) session.getAttribute("count");
-        count++;
-        session.setAttribute("count", count);
+    public static void hello(@Context HttpServletRequest req, @Context HttpServletResponse resp) throws ServletException, IOException {
+        //HttpSession session = req.getSession();
+        //int count = session.getAttribute("count") == null ? 0 : (int) session.getAttribute("count");
+        //count++;
+        //session.setAttribute("count", count);
 
         // Dispatcher vers la vue "hello.html"
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/hello.html");
