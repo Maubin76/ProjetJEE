9a0bd80661feddc538abb93dd319a8453a8ca15b
Mathieu Aubin <maubin@enssat.fr>
2024-03-12 12:19:57 +0100

    following of event management

--- src/main/java/Controllers/EventController.java
+++ src/main/java/Controllers/EventController.java
@@ -1,17 +1,32 @@
 package Controllers;
 
 import java.util.*;
+import java.io.IOException;
 import java.sql.Date;
 
 import Models.Event;
 import Models.Lieu;
 import Models.DJ;
 import Models.StyleMusical;
+import jakarta.servlet.RequestDispatcher;
 //import Queries.EventDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 
+@Path("/event-management")
 public class EventController {
 
-	public void ShowNextMonthEvents() {
+	@GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("list")
+	public void ShowNextMonthEvents(@Context HttpServletRequest req, @Context HttpServletResponse resp) throws ServletException, IOException{ 
 		Date dateMin = new Date(System.currentTimeMillis()); // Date d'aujourd'hui
 		Date dateMax = addDays(dateMin, 30);
 		// List<Event> eventList = EventDAO.EventListBetweenDates(dateMin, dateMin);
@@ -23,6 +38,11 @@ public class EventController {
 		eventList.add(new Event(dj, lieu, addDays(dateMin, -10)));
 		eventList.add(new Event(dj, lieu, addDays(dateMin, -100)));
 		
+        req.setAttribute("eventsList", eventList); // Ajoutez la liste des événements à l'objet HttpServletRequest
+        HttpSession session = req.getSession(); // Obtenez la session
+        session.setAttribute("eventsList", eventList); // Ajoutez la liste des événements à la session (optionnel)
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/nextMonthEvent.html");
+        dispatcher.forward(req, resp);
 	}
 	
     public static Date addDays(Date date, int days) {
