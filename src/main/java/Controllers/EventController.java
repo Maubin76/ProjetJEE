7becd02dfc11ab14608cd3c40c0563e67260b326
Mathieu Aubin <maubin@enssat.fr>
2024-03-12 12:23:17 +0100

    Adapt to Event class modifications

--- src/main/java/Controllers/EventController.java
+++ src/main/java/Controllers/EventController.java
@@ -3,6 +3,7 @@ package Controllers;
 import java.util.*;
 import java.io.IOException;
 import java.sql.Date;
+import java.time.LocalTime;
 
 import Models.Event;
 import Models.Lieu;
@@ -35,8 +36,8 @@ public class EventController {
 		DJ dj = new DJ("nomDJ", "prenomDJ", "nomDeSceneDJ", addDays(dateMin, -10000), "residenceDJ", StyleMusical.Electro);
 		Lieu lieu = new Lieu("nomLieu", "villeLieu", "paysLieu", "continentLieu");
 		List<Event> eventList = new ArrayList<>();
-		eventList.add(new Event(dj, lieu, addDays(dateMin, -10)));
-		eventList.add(new Event(dj, lieu, addDays(dateMin, -100)));
+		eventList.add(new Event(dj, lieu, addDays(dateMin, -10), LocalTime.parse("20:00:00"), LocalTime.parse("23:30:00")));
+		eventList.add(new Event(dj, lieu, addDays(dateMin, -100), LocalTime.parse("18:00:00"), LocalTime.parse("22:00:00")));
 		
         req.setAttribute("eventsList", eventList); // Ajoutez la liste des événements à l'objet HttpServletRequest
         HttpSession session = req.getSession(); // Obtenez la session
