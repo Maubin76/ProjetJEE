6b30ac31fb21f8f6cfe1c3fd04f96b39de12466f
TheoMon <tmontel@enssat.fr>
2024-03-13 08:52:48 +0100

    Jsp

--- src/main/java/Controllers/DJController.java
+++ src/main/java/Controllers/DJController.java
@@ -28,98 +28,98 @@ public class DJController {
 	}
 	private DJDAO djDao = new DJDAOImpl();
 		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/djs")
-		public String getDjs() {
-			List<DJ> djs;
-			djs = djDao.findByAll();
-			GsonBuilder builder = new GsonBuilder();
-			Gson gson = builder.create();
-			String json=gson.toJson(djs);
-			return json;	
-		}
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/djs")
+	public String getDjs() {
+		List<DJ> djs;
+		djs = djDao.findByAll();
+		GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.create();
+		String json=gson.toJson(djs);
+		return json;	
+	}
 		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/modification")
-		public String getDjs(@QueryParam("name") String nomScene) {
-			List<DJ> djs;
-			if (nomScene!=null) {
-				djs = djDao.findByNomDeScene(nomScene);
-			}else {
-				djs = null;
-			}
-			GsonBuilder builder = new GsonBuilder();
-			Gson gson = builder.create();
-			String json=gson.toJson(djs);
-			return json;	
-		}
-		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/supprimer")
-		public void deleteDJs(@QueryParam("nom") String nomScene) {
-			List<DJ> djs;
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/modification")
+	public String getDjs(@QueryParam("name") String nomScene) {
+		List<DJ> djs;
+		if (nomScene!=null) {
 			djs = djDao.findByNomDeScene(nomScene);
-			Iterator<DJ> iterateur = djs.iterator();
-			while (iterateur.hasNext()){
-				DJ djactuel= iterateur.next();
-				djDao.deleteFromDB(djactuel);
-			}
+		}else {
+			djs = null;
 		}
+		GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.create();
+		String json=gson.toJson(djs);
+		return json;	
+	}
 		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/ajout")
-		public void modifyDJs(@QueryParam("nom") String nomScene,
-				@QueryParam("champ") String champ,
-				@QueryParam("modification") String modification) {
-			List<DJ> djs;
-			djs = djDao.findByNomDeScene(nomScene);
-			Iterator<DJ> iterateur = djs.iterator();
-			while (iterateur.hasNext()){
-				DJ djactuel= iterateur.next();
-				djDao.modifyDJ(djactuel,champ,modification);
-			}
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/supprimer")
+	public void deleteDJs(@QueryParam("nom") String nomScene) {
+		List<DJ> djs;
+		djs = djDao.findByNomDeScene(nomScene);
+		Iterator<DJ> iterateur = djs.iterator();
+		while (iterateur.hasNext()){
+			DJ djactuel= iterateur.next();
+			djDao.deleteFromDB(djactuel);
 		}
-		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/ajout")
-		public void addDJs(@QueryParam("nom") String nom,
-				@QueryParam("prenom") String prenom,
-				@QueryParam("dateDeNaissance") String dateDeNaissance,
-				@QueryParam("nomDeScene") String nomDeScene,
-				@QueryParam("lieuDeResidence") String lieuDeResidence,
-				@QueryParam("styleDeMusique") int styleDeMusique) {
-			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-			Date dateNaissance = null;
-			try {
-				java.util.Date utilDate = dateFormat.parse(dateDeNaissance);
-				java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-				dateNaissance = sqlDate;
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			StyleMusical styleMusical = null;
-			switch (styleDeMusique) {
-            case 1:
-            	styleMusical=StyleMusical.Electro;
-                break;
-            case 2:
-            	styleMusical=StyleMusical.House;
-                break;
-            case 3:
-            	styleMusical=StyleMusical.Hard_style;
-                break;
-            case 4:
-            	styleMusical=StyleMusical.EDM;
-            	break;
-        }
-			DJ dj= new DJ(nom,prenom,nomDeScene,dateNaissance,lieuDeResidence,styleMusical);
-			djDao.insertDJtoDB(dj);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/ajout")
+	public void addDJs(@QueryParam("nom") String nom,
+			@QueryParam("prenom") String prenom,
+			@QueryParam("dateDeNaissance") String dateDeNaissance,
+			@QueryParam("nomDeScene") String nomDeScene,
+			@QueryParam("lieuDeResidence") String lieuDeResidence,
+			@QueryParam("styleDeMusique") int styleDeMusique) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateNaissance = null;
+		try {
+			java.util.Date utilDate = dateFormat.parse(dateDeNaissance);
+			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+			dateNaissance = sqlDate;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		StyleMusical styleMusical = null;
+		switch (styleDeMusique) {
+        case 1:
+           	styleMusical=StyleMusical.Electro;
+            break;
+         case 2:
+            styleMusical=StyleMusical.House;
+            break;
+         case 3:
+            styleMusical=StyleMusical.Hard_style;
+            break;
+         case 4:
+            styleMusical=StyleMusical.EDM;
+            break;
+       }
+		DJ dj= new DJ(nom,prenom,nomDeScene,dateNaissance,lieuDeResidence,styleMusical);
+		djDao.insertDJtoDB(dj);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/ajout")
+	public void modifyDJs(@QueryParam("nom") String nomScene,
+			@QueryParam("champ") String champ,
+			@QueryParam("modification") String modification) {
+		List<DJ> djs;
+		djs = djDao.findByNomDeScene(nomScene);
+		Iterator<DJ> iterateur = djs.iterator();
+		while (iterateur.hasNext()){
+			DJ djactuel= iterateur.next();
+			djDao.modifyDJ(djactuel,champ,modification);
+		}
+	}
 		
 }
