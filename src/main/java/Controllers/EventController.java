07e362658535d9be697028720cb6ef90deb5bd05
TheoMon <tmontel@enssat.fr>
2024-04-04 17:40:05 +0200

    Commit avec probleme

--- src/main/java/Controllers/EventController.java
+++ src/main/java/Controllers/EventController.java
@@ -20,7 +20,13 @@ import Queries.DJDAO;
 import Queries.DJDAOImpl;
 import Queries.EventDAO;
 import Queries.EventDAOImpl;
-//import Queries.EventDAO;
+import Queries.ClubDAO;
+import Queries.ClubDAOImpl;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
@@ -55,6 +61,8 @@ public class EventController {
 
     private EventDAO eventDao = new EventDAOImpl(); // Instance du DAO pour la gestion des événements
     private DJDAO djDao = new DJDAOImpl(); // Instance du DAO pour la gestion des DJs
+    private ClubDAO clubDao = new ClubDAOImpl(); // Instance du DAO pour la gestion des lieus
+
 
     // Méthode pour ajouter un certain nombre de jours à une date
     public static Date addDays(Date date, int days) {
@@ -77,51 +85,64 @@ public class EventController {
 		List<Event> eventList = new ArrayList<>();
 		List<EventJSON> eventListJSON=new ArrayList<>();
 		eventList = eventDao.eventListBetweenDates(dateMin,dateMax);
-		for(Event event:eventList) {
-			eventListJSON.add(new EventJSON(event.getNom(), event.getDj().getNomDeScene(),event.getLieu().getNomLieu(),event.getDate(),event.getHoraireDebut(),event.getHoraireFin()));
-		}
+		for (Event event : eventList) {
+        	if(event.getDj()==null) {
+        		eventListJSON.add(new EventJSON(event.getNom(), "Pas de DJ pour cet event", event.getLieu().getNomLieu(), event.getDate(), event.getHoraireDebut(), event.getHoraireFin()));
+        	}else {
+        		eventListJSON.add(new EventJSON(event.getNom(), event.getDj().getNomDeScene(), event.getLieu().getNomLieu(), event.getDate(), event.getHoraireDebut(), event.getHoraireFin()));
+        	}
+        }
     	GsonBuilder builder = new GsonBuilder();
 		Gson gson = builder.create();
 		String json=gson.toJson(eventListJSON);
 		return json;
     }
 
-    // Endpoint pour ajouter un DJ à un événement
+  //Endpoint pour ajouter un événement
     @POST
-    @Path("/ajoute")
+    @Path("/ajoutEvent")
     @Consumes("application/x-www-form-urlencoded")
-    public void addDjs(@FormParam("evenement") String nomEvenement, @FormParam("dj") String nomSceneDJ) {
-        DJ dj = djDao.findByNomDeScene(nomSceneDJ).get(0); // Récupération du DJ par son nom de scène
-        Event evenement = eventDao.findByNom(nomEvenement).get(0); // Récupération de l'événement par son nom
-        DJ djAssigne = evenement.getDj(); // Récupération du DJ déjà assigné à l'événement
-        // Vérification si le DJ et l'événement existent et si aucun DJ n'est déjà assigné à l'événement
-        if (dj != null && evenement != null && djAssigne == null) {
-            eventDao.addDjtoEvent(dj, evenement); // Ajout du DJ à l'événement dans la base de données
-        }
-    }
-    
-    // Endpoint d'affichage des events d'un mois précis
-    // Le mois est celui de la date (String) placée en paramètre
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/ShowEvents")
-    public String ShowEvents(@QueryParam("dateString") String dateString) {
-        java.sql.Date sqlDate = new Date(System.currentTimeMillis());
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
+    public void addEvent(@FormParam("nomEvent") String nomEvent, 
+    		@FormParam("nomLieu") String nomLieu,
+    		@FormParam("dateEvent") String dateEvent,
+    		@FormParam("heureDebut") String heureDebut,
+    		@FormParam("heureFin") String heureFin) {
+    	
+    	
+    	Lieu lieu = clubDao.findByName(nomLieu);
+    	
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateEvenement = null;
+        Time heureDebutObjet = null;
+        Time heureFinObjet = null;
+
         try {
-            // Parsing de la chaîne en java.util.Date
-            java.util.Date parsedDate = dateFormat.parse(dateString);
-
-            // Conversion de java.util.Date en java.sql.Date
-            sqlDate = new java.sql.Date(parsedDate.getTime());
-
-            // Affichage de la date SQL
-            System.out.println("Date SQL : " + sqlDate);
+            java.util.Date utilDate = dateFormat.parse(dateEvent);
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            dateEvenement = sqlDate;
         } catch (ParseException e) {
-            // Gestion des erreurs de parsing
             e.printStackTrace();
         }
-        return getEventsOfTheMonth(sqlDate);
+        // Définir le format de l'heure
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        try {
+        	// Convertir la Date en objet Time
+            heureDebutObjet = new Time(sdf.parse(heureDebut).getTime());
+            heureFinObjet = new Time(sdf.parse(heureFin).getTime());
+           } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Event event = new Event(nomEvent, null,lieu,dateEvenement,heureDebutObjet,heureFinObjet);
+        eventDao.insertEventtoDB(event);
     }
+    
+    @GET
+    @Path("/eventDispo")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void getEventsDispo(@QueryParam("nomDeSceneDJ") String nomEvent) {
+    	
+    }
+
 }
 
